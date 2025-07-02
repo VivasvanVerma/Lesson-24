0840e3403dbda81f4ff786168737b15333bc0cd0
VivasvanVerma <vvofficial.yt@gmail.com>
2025-07-02 18:16:44 +0530

    sets

---
+++ sets.py
@@ -0,0 +1,17 @@
+my_set = {1,2,3}
+print(my_set)
+
+
+set1 = {73, 'hello', (1,2,3)}
+print(set1)
+
+set2 = {1,2,3,2}
+print(set2)
+
+set3 = set([1,2,3,4,3])
+print(set3)
+
+set4 = set([0,1,3,4,5])
+print("Original set: ", set4)
+set4.pop()
+print("After removing 1st term: ", set4)
